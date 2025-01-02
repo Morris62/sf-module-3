b6f655c5893e7cfc049916997a4845978b3c224d
a.chaturov <chaturovai@gmail.com>
2025-01-02 23:15:53 +0300

    3.3.1

--- Module 3/Program.cs
+++ Module 3/Program.cs
@@ -4,23 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        const string myName = "Jane";
-        Console.WriteLine(myName);
-
-        Console.WriteLine("\t Привет, мир");
-        Console.WriteLine("\t Мне 41 год");
-        Console.WriteLine("\t My name is \n {0}", myName);
-        Console.WriteLine("\x0040");
-        Console.WriteLine("\x23");
+        string MyName = "Jane";
+        byte MyAge = 27;
+        bool HaveIPet = true;
+        double MyShoeSize = 37.5;
         
-        Console.WriteLine(true);
-        Console.WriteLine(false);
-
-        Console.WriteLine(5);
-        
-        Console.WriteLine(0x0A);
-        Console.WriteLine(0b11);
-        Console.WriteLine(5.5);
+        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("My age is " + MyAge);
+        Console.WriteLine("Do I have a pet? " + HaveIPet);
+        Console.WriteLine("My shoe size is " + MyShoeSize);
         
         Console.ReadKey();
     }
