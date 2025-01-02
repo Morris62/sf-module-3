af9b7d78f1a31c96d840eecaf0a6976c05967552
a.chaturov <chaturovai@gmail.com>
2025-01-02 23:35:32 +0300

    3.4.1

--- Module 3/Program.cs
+++ Module 3/Program.cs
@@ -4,21 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        string MyName = "Jane";
-        byte MyAge = 27;
-        bool HaveIPet = true;
-        double MyShoeSize = 37.5;
-        
-        Console.WriteLine("My name is " + MyName);
-        Console.WriteLine("My age is " + MyAge);
-        Console.WriteLine("Do I have a pet? " + HaveIPet);
-        Console.WriteLine("My shoe size is " + MyShoeSize);
+        DaysOfWeek MyFavouriteDay = DaysOfWeek.Friday;
 
-        Console.WriteLine("Int.MaxValue {0}", int.MaxValue);
-        Console.WriteLine("Int.MinValue {0}", int.MinValue);
-        Console.WriteLine("Double.MaxValue {0}", double.MaxValue);
-        Console.WriteLine("Double.MinValue {0}", double.MinValue);
+        Console.WriteLine(MyFavouriteDay);
         
         Console.ReadKey();
+    }
+
+    enum DaysOfWeek : byte
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
     }
 }
