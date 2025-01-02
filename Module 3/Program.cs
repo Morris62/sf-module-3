fd9418166ccf78b04407584e0a6d9d5a95946972
a.chaturov <chaturovai@gmail.com>
2025-01-02 22:49:39 +0300

    3.2.8

--- Module 3/Program.cs
+++ Module 3/Program.cs
@@ -10,7 +10,17 @@ class Program
         Console.WriteLine("\t Привет, мир");
         Console.WriteLine("\t Мне 41 год");
         Console.WriteLine("\t My name is \n {0}", myName);
-        Console.WriteLine("\u0040");
+        Console.WriteLine("\x0040");
+        Console.WriteLine("\x23");
+        
+        Console.WriteLine(true);
+        Console.WriteLine(false);
+
+        Console.WriteLine(5);
+        
+        Console.WriteLine(0x0A);
+        Console.WriteLine(0b11);
+        Console.WriteLine(5.5);
         
         Console.ReadKey();
     }
